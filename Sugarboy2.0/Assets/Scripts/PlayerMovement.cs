337093d5bbb0f259d82542f401defd91dc5bc58b
Artis <artux28@hotmail.com>
2020-11-13 17:40:26 +0100

    camera switches to new clone, old clones cannot move

--- Sugarboy2.0/Assets/Scripts/PlayerMovement.cs
+++ Sugarboy2.0/Assets/Scripts/PlayerMovement.cs
@@ -2,50 +2,68 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
     [SerializeField] private float _movementSpeed = 10f;
     [SerializeField] public bool _isGrounded = false;
     [SerializeField] private float _jumpingSpeed = 5f;
     [SerializeField] private LayerMask platformLayerMask;
     [SerializeField] private float maxDistance = 1.5f;
-    RaycastHit hit;
-    private Vector3 direction;
     [SerializeField] private bool hitDetected;
     private BoxCollider boxCollider;
-    Rigidbody rb;
 
-    private void Awake () {
-        boxCollider = gameObject.GetComponent<BoxCollider> ();
-        rb = this.GetComponent<Rigidbody> ();
-    }
-    private void FixedUpdate () {
-        //Both ways of movement do basically the same thing. I have no idea why the movement occasionally becomes jittery or by moving against some other object too. TODO -> need to figure out why that happens
-
-        transform.Translate (Vector3.Normalize (Vector3.right) * _movementSpeed * Time.deltaTime * Input.GetAxis ("Horizontal"));
-
-        // rb.MovePosition ((transform.position + (direction * _movementSpeed * Time.deltaTime)));
+    private void Awake()
+    {
+        boxCollider = gameObject.GetComponent<BoxCollider>();
     }
 
-    private void Update () {
-        direction = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0, 0);
-        Jump ();
-    }
-    private void Jump () {
-        if (IsGrounded () && Input.GetButtonDown ("Jump")) {
-            gameObject.GetComponent<Rigidbody> ().AddForce (new Vector2 (0f, _jumpingSpeed), ForceMode.Impulse);
+    private void FixedUpdate()
+    {
+        if (CheckActive())
+        {
+            Movement();
         }
     }
 
-    private bool IsGrounded () {
+    private void Movement()
+    {
+        transform.Translate(Vector3.Normalize(Vector3.right) * _movementSpeed * Time.deltaTime * Input.GetAxis("Horizontal"));
+    }
 
-        hitDetected = Physics.BoxCast (new Vector3 (transform.position.x, transform.position.y, transform.position.z),
-            new Vector3 ((boxCollider.size.x * 0.9f), boxCollider.size.y, (boxCollider.size.z * 0.9f)) * 0.5f, -Vector3.up,
-            out hit,
-            boxCollider.transform.rotation,
-            maxDistance
-        );
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            this.tag = "Clone";
+        }
+        if (CheckActive())
+        {
+            Jump();
+        }
+    }
+
+    private bool CheckActive()
+    {
+        return this.CompareTag("Player");
+    }
+
+    private void Jump()
+    {
+        if (IsGrounded() && Input.GetButtonDown("Jump"))
+        {
+            gameObject.GetComponent<Rigidbody>().AddForce(new Vector2(0f, _jumpingSpeed), ForceMode.Impulse);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        hitDetected = Physics.BoxCast(new Vector3(transform.position.x, transform.position.y, transform.position.z),
+                new Vector3((boxCollider.size.x * 0.9f), boxCollider.size.y, (boxCollider.size.z * 0.9f)) * 0.5f, -Vector3.up,
+                out _,
+                boxCollider.transform.rotation,
+                maxDistance
+            );
 
         return hitDetected;
     }
-
 }
