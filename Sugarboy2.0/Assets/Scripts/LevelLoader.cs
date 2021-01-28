841711df12b047a5dbe0f8dcd90fade5e3d79f50
Digel <dann-david@t-online.de>
2021-01-28 12:06:59 +0100

    TransitionPrototyp

---
+++ Sugarboy2.0/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelLoader : MonoBehaviour
+{
+
+    private Text messageText;
+
+    private void Awake()
+    {
+        messageText = transform.Find("Transition").Find("Image").Find("messageText").GetComponent<Text>();
+    }
+
+    private void Start()
+    {
+        TextWriter.AddWriter_Static(messageText, "Tutorial 1", .1f, true);
+    }
+
+}
