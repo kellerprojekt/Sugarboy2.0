8d9374979014ad31efc701924f52279c93654776
Digel <dann-david@t-online.de>
2021-02-01 17:45:07 +0100

    added Sounds and Transitions

---
+++ Sugarboy2.0/Assets/Scripts/ElectricSound.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElectricSound : MonoBehaviour
+{
+    public AudioSource[] clip = new AudioSource[3];
+    [SerializeField] private float timeBetween;
+    [Range(0, 1)] [SerializeField] private float volume;
+
+    private void Start()
+    {
+        InvokeRepeating("RepeatSound", 1f, timeBetween);
+    }
+
+    private int PlayRandomSound()
+    {
+        int numb = Random.Range(0, 3);
+        return numb;
+    }
+
+    private void RepeatSound()
+    {
+        clip[PlayRandomSound()].Play();
+    }
+}
--- Sugarboy2.0/Assets/Scripts/LevelLoader.cs
+++ Sugarboy2.0/Assets/Scripts/LevelLoader.cs
@@ -1,21 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class LevelLoader : MonoBehaviour
 {
 
     private Text messageText;
+    public Animator transition;
+    public float transitionTime = 1f;
+    public List<string> introTexts = new List<string>();
 
     private void Awake()
     {
         messageText = transform.Find("Transition").Find("Image").Find("messageText").GetComponent<Text>();
+        introTexts.Add("Welcome");
+        introTexts.Add("Tutorial 1");
+        introTexts.Add("Tutorial 2");
+        introTexts.Add("Level 1");
+        introTexts.Add("Level 2");
     }
 
     private void Start()
     {
-        TextWriter.AddWriter_Static(messageText, "Tutorial 1", .1f, true);
+        TextWriter.AddWriter_Static(messageText, introTexts[SceneManager.GetActiveScene().buildIndex], .1f, true);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        LoadNextLevel();
+    }
+
+    public void LoadNextLevel()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        transition.SetTrigger("EndLevel");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(levelIndex);
     }
 
 }
