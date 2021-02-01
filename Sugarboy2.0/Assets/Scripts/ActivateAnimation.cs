c39acdfd03c30efa5428a33397fbe5b66a32c787
Artis <artux28@hotmail.com>
2021-02-01 13:06:29 +0100

    level collisions

---
+++ Sugarboy2.0/Assets/Scripts/ActivateAnimation.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateAnimation : MonoBehaviour
+{
+    private Animator anim;
+
+    //private bool activated = false;
+    [SerializeField] private GameObject elevator;
+
+    private void Awake()
+    {
+        anim = elevator.GetComponent<Animator>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag.Contains("Player"))
+        {
+            if (anim.enabled == false)
+            {
+                anim.enabled = true;
+            }
+            anim.SetBool("activated", true);
+        }
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.tag.Contains("Player"))
+        {
+            anim.enabled = false;
+            anim.SetBool("activated", false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Contains("Player"))
+        {
+            if (anim.enabled == false)
+            {
+                anim.enabled = true;
+            }
+            anim.SetBool("activated", true);
+        }
+    }
+}
